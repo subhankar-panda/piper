fea83b1e1a683d29f8d7de3076197633745c4d56
Subhankar Panda <s1panda@ucsd.edu>
2017-07-23 14:58:54 +0530

    bleh

--- server/main.go
+++ server/main.go
@@ -11,7 +11,7 @@ func main() {
 
     PORT := os.Getenv("PORT")
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, r.URL.Path[1:])
+        fmt.Println(w, "Hello World")
     })
 
     http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
