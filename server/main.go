5e733b7e221359068828faa1def22497cb4f2088
Subhankar Panda <s1panda@ucsd.edu>
2017-07-25 21:27:01 +0530

    testing db

--- server/main.go
+++ server/main.go
@@ -66,9 +66,9 @@ func inputHandler(w http.ResponseWriter, req *http.Request) {
 
     sess.SetSafe(&mgo.Safe{})
 
-//    c := sess.DB("piper").C("pipes")
+    c := sess.DB("piper").C("pipes")
 
-
+    fmt.Fprintln("databases")
 }
 
 
